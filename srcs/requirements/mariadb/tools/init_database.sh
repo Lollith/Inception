607e0f8762be290cc730ea7bdbc0d6d77e90b23f
agouet <agouet@Xubuntu.myguest.virtualbox.org>
2023-05-31 16:15:06 +0200

    debug

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 service mysql start
+
+sleep 3 
+# Assurez-vous que le service MySQL est correctement démarré avant d'exécuter les commandes MySQL. Vous pouvez ajouter une pause ou une vérification pour vous assurer que le service est actif avant d'exécuter les commandes suivantes.
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
+exec mysqld
 # mysql  my_script.sql
-service mysql status;
+# service mysql status;
--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # // condition si wp-config.php nexiste pas alors lance la commade wp config create
+sleep 10
 
-wp config create --allow-root 	--dbname="ade" \
-											--dbuser="ade" \
-											--path='/var/www/wordpress'
-											# --dbpass=$SQL_PASSWORD \
+wp config create	--allow-root \
+					--dbname=$MYSQL_DATABASE \
+					--dbuser=$MYSQL_USER \
+					--dbpass=$MYSQL_PASSWORD \
+					--dbhost=mariadb:3306 \
+					--path='/var/www/wordpress'
+
+# run php-fpm:
+# exec php-fpm7.3 -F
