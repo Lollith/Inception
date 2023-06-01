a1b06b1acc0c023b6e25149411f8bbea622c3b98
agouet <agouet@Xubuntu.myguest.virtualbox.org>
2023-06-01 19:55:53 +0200

    debug

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -8,10 +8,10 @@ sleep 3
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# # mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 # exec mysqld_safe
 exec mysqld
 # mysql  my_script.sql
