edc7642549f01bff9483b001f2262686964dd958
agouet42 <agouet42@maison.myguest.virtualbox.org>
2023-07-26 21:25:40 +0200

    erreur db

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -40,15 +40,15 @@ if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 
     fi
         mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-        mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-        mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+        mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+        mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
         mysql -e "FLUSH PRIVILEGES;"
         mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
 
         #ferme le processus enfant
         mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
-        #exec remplace le pid1 du .sh , devient nouveau processus principal
 fi
+    #exec remplace le pid1 du .sh , devient nouveau processus principal
     exec mysqld --user=root
 #debug
 set +x
--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -3,22 +3,25 @@
 set -x
 #check si wp-config.php pas deja cree
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	# sleep 20
 
 	/usr/local/bin/wp-cli.phar  core download --allow-root --path=/var/www/wordpress
 							
+	# sleep 40
+	./wait
 
 	#configurtion  de wordpress a son lancement
 	/usr/local/bin/wp-cli.phar config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path=/var/www/wordpress
 
-
+	# sleep 40
 	#1er utilisateur 
-	/usr/local/bin/wp-cli.phar core install --allow-root \
-					--url=$DOMAIN_NAME \
-					--title=$WP_TITLE \
-					--admin_user=$WP_ADMIN \
-					--admin_password=$WP_ADMIN_PASSWORD \
-					--admin_email=$WP_ADMIN_EMAIL \
-					--path=/var/www/wordpress
+	# /usr/local/bin/wp-cli.phar core install --allow-root \
+	# 				--url=$DOMAIN_NAME \
+	# 				--title=$WP_TITLE \
+	# 				--admin_user=$WP_ADMIN \
+	# 				--admin_password=$WP_ADMIN_PASSWORD \
+	# 				--admin_email=$WP_ADMIN_EMAIL \
+	# 				--path=/var/www/wordpress
 
 	#2eme utilisateur
 	# wp user create --allow-root \
